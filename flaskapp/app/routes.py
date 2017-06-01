3b4b69ebe14eda79cef188c5066c1ae45f202952
heliopn <hpaivan@gmail.com>
2017-06-01 20:17:37 -0300

    Add files via upload
    Update

--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
-from firebase import firebase
-from flask import Flask
-from .forms import FirePut
+  
 app = Flask(__name__)
-firebase = firebase.FirebaseApplication("<https://marmitech-1b071.firebaseio.com>", None)
- 
+  
 @app.route('/')
 def home():
-  return render_template('home.html')
-@app.route('/about')
-def about():
-  return render_template('about.html') 
+  return render_template('PaginaHome.html')
+  
+@app.route('/cadastro')
+def cadastro():
+  return render_template('PaginaCadastroUser.html')
+
+@app.route('/cadastrovend')
+def cadastrovend():
+  return render_template('PaginaCadastroVendedor.html')
+    
 if __name__ == '__main__':
   app.run(debug=True)
