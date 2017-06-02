66a5ccbb2979adedb77af81d19bcc8fd9499b657
iChaos26 <joony-26@hotmail.com>
2017-06-02 01:57:09 -0300

    Incompleto
    Faltam definir botões, interface usuario e interface vendedor.

--- dictcomidas.py
+++ dictcomidas.py
@@ -1,31 +1,39 @@
 from firebase import firebase
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory
+import os
 
-Marmitech=firebase.FirebaseApplication('https://marmitech-1b071.firebaseio.com', None)
 
+Marmitech = firebase.FirebaseApplication('https://marmitech-1b071.firebaseio.com/')
+usuario=[]
+vendedor=[]
+nomedapessoa=[]
 email=[]
-comida1=[]
-comida2=[]
-comida3=[]
-pedido1=[]
-pedido2=[]
-pedido3=[]
+comida=[]
+# pedido=[]
+pedidototal=[]
 
 
-
-class Vendedores ():
+class Vendedor ():
 	def __init__ (self,nomedapessoa,email,senha):
 		self.nomedapessoa= nomedapessoa
 		self.email=email
 		self.senha= senha
-		self.comida1=[]
-		self.comida2=[]
-		self.comida3=[]
+		self.comida=[]
 		self.dictvendedores={}
 
-	def SalvarVendedores(self,nom):
-		self.dictvendedores[self.nomedapessoa]=self.nomedapessoa,self.email,self.senha,self.comida1,self.comida2,self.comida3
-		email.append(self.email)
-		return self.dictvendedores
+	def SalvarVendedor(self):
+		# self.dictvendedor[self.nomedapessoa]=[self.email,self.senha,self.comida]
+		# listaVendedores = []
+		# listaVendedores.append(self.comida)
+		# listaVendedores.append(self.senha)	
+		# listaVendedores.append(self.email)
+		# return self.dictvendedores
+		self.dictvendedor["nomedovendedor"]=self.nomedovendedor
+		self.dictvendedor["email"]=self.email
+		self.dictvendedor["senha"]=self.senha
+		self.dictvendedor["pedidototal"]=self.pedidototal
+		telavendedor=Marmitech.post('marmitech-1b071',vendedor[0].SalvarVendedor())
+
 
 class Cliente():
 	def __init__(self,nomedapessoa,email,senha):
@@ -33,22 +41,92 @@ class Cliente():
 		self.email=email
 		self.senha=senha
 		self.pedidototal=pedidototal
-		self.pedido1=[]
-		self.pedido2=[]
-		self.pedido3=[]
 		self.dictcliente={}	
 	
 	def SalvarCliente(self):
-		self.dictcliente[self.pedidototal]=self.pedidototal,self.pedidototal.append(self.pedido1),self.pedidototal.append(self.pedido2),self.pedidototal.append(self.pedido3),self.senha,self.nomedapessoa
-		email.append(self.email)
-		dictcliente.append(self.pedidototal)
-		return self.dictcliente
+		# self.dictcliente[self.pedidototal]=self.pedidototal,self.senha,self.nomedapessoa
+		# listaClientes = []
+		# listaVendedores.append(self.pedidototal)
+		# listaVendedores.append(self.senha)	
+		# listaVendedores.append(self.email)
+		# return self.dictcliente
+		self.dictcliente["nomedapessoa"]=self.nomedapessoa
+		self.dictcliente["email"]=self.email
+		self.dictcliente["senha"]=self.senha
+		self.dictcliente["pedidototal"]=self.pedidototal
+		telacliente=Marmitech.post('marmitech-1b071',usuario[0].SalvarCliente())
 
 
 
-Marmitech=firebase.FirebaseApplication('https://marmitech-1b071.firebaseio.com', None)
-# telacliente= firebase.put('marmitech-1b071',{dictcliente})
-telavendedor= firebase.post('marmitech-1b071',{dictvendedores})
+app = Flask(__name__, static_url_path='')
+
+UPLOAD_FOLDER = 'static/uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    
+@app.route('/uploads/<filename>')
+
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
 
+ @app.route('/', methods=['POST','GET'])
+
+
+
+#telacliente= firebase.put('marmitech-1b071',{dictcliente})
+#telavendedor= firebase.post('marmitech-1b071',{dictvendedores})
+def validar(): 
+	if request.method= 'POST':
+		nomedapessoa=request.form['nomedapessoa']
+		senha=request.form['senha']
+		ler=Marmitech.get('marmitech-1b071','-Kl_awU2thszeeh9okfl',None)
+
+		if nomedapessoa in ler:
+			listasenha=[]
+			ver=Marmitech.get('marmitech-1b071','-Kl_awU2thszeeh9okfl',None)
+			listasenha.append(ver)
+			if senha in listasenha:
+				return render_template('PaginaHome.html', nomedapessoa=nomedapessoa)
+			else:
+			x= 'Senha Incorreta'
+			return render_template('PaginaCadastroUser.html', nomedapessoa=nomedapessoa, dic=Marmitech.get('marmitech-1b071','-Kl_awU2thszeeh9okfl',None))	
+		else:
+			x= 'Usuario Inválivo'
+			return render_template('PaginaCadastroUser.html', nomedapessoa=nomedapessoa, dic='PaginaCadastroUser.html')	
+	return render_template('PaginaHome.html')
+	
+
+@app.route('/login', methods=['POST','GET'])	
+
+def perfil():
+	usuario=request.args['usuario']
+	try:
+		
+
+
+
+
+
+
+# #listaVendedores = []
+# listausuario=[]
+# listasenha=[]
+# #vend = Vendedor(xyz,"antonio@hotmail.com","1234")
+# v=Vendedor(usu,sen,)
+# usu=Vendedor("helio")
+# # listaVendedores.append(vend)
+# listasenha.append(sen)
+# listausuario.append(usu)
+
+
+# # vend = Vendedor("joao","joao@hotmail.com","12345")
+# # listaVendedores.append(vend)
+# telavendedor=Marmitech.post('marmitech-1b071',listausuario[0].SalvarVendedor())
+
+# telavendedor = Marmitech.post('marmitech-1b071',listasenha[0].SalvarVendedor())
